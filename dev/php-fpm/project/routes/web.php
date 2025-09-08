8fe2091fc1efdf95224137fc8da977004d8ce1a2
Oscar <cedano@engineer.com>
2025-09-07 23:33:03 -0700

    feat: adding official redisinsight

--- dev/php-fpm/project/routes/web.php
+++ dev/php-fpm/project/routes/web.php
@@ -1,11 +1,6 @@
 <?php
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/auth/login', function(UserController $user) {
-    return $user->login();
-});
 
 Route::prefix('/auth')->group(function() {
     

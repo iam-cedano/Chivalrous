34f86d5291967f9b2fd01303a74e6ca4a939956d
Oscar <cedano@engineer.com>
2025-10-28 00:20:16 -0700

    feat: adding react-router

--- dev/php-fpm/project/resources/js/pages/client/Client.tsx
+++ dev/php-fpm/project/resources/js/pages/client/Client.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { Footer } from "./routes/home/components/Client.Footer";
+import { BrowserRouter, Route } from "react-router";
+
 
 ReactDOM.createRoot(document.getElementById("app")!)
     .render(
         <StrictMode>
             
-
+            <BrowserRouter>
+                <Route  />
+            </BrowserRouter>
+            
+            <Footer />
         </StrictMode>
     );
     
--- dev/php-fpm/project/resources/js/pages/client/routes/home/components/Client.Footer.tsx
+++ dev/php-fpm/project/resources/js/pages/client/routes/home/components/Client.Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <nav id="footer" className="w-full p-[10px] fixed inset-x-0 bottom-0 flex">
-
+            <p>Footer</p>
         </nav>
     );
 }

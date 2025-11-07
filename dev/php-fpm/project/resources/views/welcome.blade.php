13594398c3695873182f986f441c5d50e5461966
Oscar <cedano@engineer.com>
2025-11-06 21:24:26 -0600

    feat: adding favicon

--- dev/php-fpm/project/resources/views/welcome.blade.php
+++ dev/php-fpm/project/resources/views/welcome.blade.php
@@ -3,6 +3,13 @@
 @viteReactRefresh
 @vite('resources/js/app.tsx')
 
+    <link rel="icon" type="image/png" href="/build/assets/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/build/assets/favicon.svg" />
+    <link rel="shortcut icon" href="/build/assets/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/build/assets/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Chivalrous" />
+    <link rel="manifest" href="/build/assets/site.webmanifest" />
+
 </head>
 
 <body>
